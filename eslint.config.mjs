2f51a7b7f30e383ffea1d73cafd9e4102b8cff92
BlackiePearlJoobi <katze526reiswein@gmail.com>
2025-07-07 16:29:01 -0500

    Updated eslint.congif.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,9 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: ["app/generated/prisma/**"], // ignore Prisma's generated output
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
